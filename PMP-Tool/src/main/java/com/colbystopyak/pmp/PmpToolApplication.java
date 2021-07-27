535506890afe77b810c3bfa36164069f2ebe52d3
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-07-27 14:03:37 -0500

    starting project

---
+++ PMP-Tool/src/main/java/com/colbystopyak/pmp/PmpToolApplication.java
@@ -0,0 +1,13 @@
+package com.colbystopyak.pmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmpToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmpToolApplication.class, args);
+	}
+
+}
